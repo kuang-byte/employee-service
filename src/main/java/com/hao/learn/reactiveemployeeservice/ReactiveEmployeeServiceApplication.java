4aee8bc4872e819f2231bed401ae3f3376c2e14a
Hao Kuang <kuanghaochina@gmail.com>
2019-09-02 22:23:41 -0400

    First commit

---
+++ src/main/java/com/hao/learn/reactiveemployeeservice/ReactiveEmployeeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.hao.learn.reactiveemployeeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveEmployeeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveEmployeeServiceApplication.class, args);
+	}
+
+}
